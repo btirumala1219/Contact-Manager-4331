f127b9c89b629cb51e83c62cee6d3c5ca56fcd00
btirumala <barath.tirumala@gmail.com>
2018-10-19 17:29:21 -0400

    Structure

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveTask);
+
+function saveTask(e){
+  console.log("savetask is called");
+  var taskName =document.getElementById('taskName').value;
+  var taskDetails =document.getElementById('taskDetails').value;
+
+ var task = {
+   name: taskName,
+   details: taskDetails
+ }
+
+ if(localStorage.getItem('tasks') === null){
+    var tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+
+    var tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  document.getElementById('myForm').reset();
+  fetchTasks();
+  //e.preventDefault();
+}
+
+function deleteTask(url){
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  var name = url;
+  //console.log(tasks);
+
+  for(var i =0;i < tasks.length;i++){
+    console.log(i);
+    console.log(tasks[i].name);
+    if(tasks[i].name == name){
+      tasks.splice(i, 1);
+    }
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  fetchTasks();
+}
+
+function fetchTasks(){
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  var tasksResults = document.getElementById('tasksResults');
+
+  tasksResults.innerHTML = '';
+  //console.log(tasks);
+  if(tasks != null){
+    for(var i = 0; i < tasks.length ; i++){
+    var name = tasks[i].name;
+    var details = tasks[i].details;
+    tasksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+'</h3>' + details +
+                                  '<p>'+ '</br>' +'<a onclick="deleteTask(\''+name+'\')" class="btn btn-danger" href="#">Delete</a>' + '</p>' +
+                                  "_________________________________________________________________________"
+                                  '</div>';
+    }
+  }
+}
+
+
+
+// Validate Form
